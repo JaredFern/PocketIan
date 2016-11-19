4c7df3cf4bbe97b05c463886c4a606e2421b8503
conwayz <conwayzhu2019@u.northwestern.edu>
2016-11-19 01:06:52 -0600

    fix spellchecker

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    response_text = process(spell(message_text))
+                    fixed_text = spell(message_text)
+                    response_text = process(fixed_text)
                     send_message(sender_id, response_text)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
