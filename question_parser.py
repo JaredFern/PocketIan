171fa6d0589217ad54a5ceca138e481f2e1c7923
Jared Fernandez <fern.jared@gmail.com>
2016-11-19 21:12:25 -0600

    wildcard

--- question_parser.py
+++ question_parser.py
@@ -10,17 +10,18 @@ question_list= [
     "What are the prerequisites for *?",    # Course
     "What do I need to take for *?",        # Course
     "What are the * requirements?",         # major, minor, theme, honors, masters
-    "How do I declare *?",                  # major, HARD CODE
+    "How do I declare a * in EECS?",                  # major, HARD CODE
 
     "Is * offered * quarter?",              # course, quarter
+    #Is eecs 349 offered winter quarter?
     "Who teaches *?",                       # course
-    "Who teaches * during * quarter?",      # course
-    "Who teaches * next quarter?",          # course
+    # who teaches eecs 349?
+    #"Who teaches * during * quarter?",      # course
+    #"Who teaches * next quarter?",          # course
 
     "What courses count for *?",            # breadth_category
     "Is * available * quarter?",
     "What classes does * teach?",
-    "What classes count for *?",
 
     "Does * count for the project requirement?",
     "Does * count for the breadth requirement?",
@@ -37,46 +38,65 @@ question_list= [
 def questionToPattern():
     question_list_parsed = []
     for each in question_list:
-        question_list_parsed.append(re.split('[ ]', each))
+        question_list_parsed.append(re.split('[ ?]', each))
+        each = re.sub(r'[^\w\s]','',each)
     return question_list_parsed
 
 def read_tokenize(chat):
-    chat = re.sub(r'[^\w\s]','',chat) + "?"
-
+    chat = re.sub(r'[^\w\s]','',chat)
     tokenized_chat  = re.split ("[ ]", chat)
     return tokenized_chat
+
+def join_lower (str1):
+    str1 = "".join(str1)
+    str1.lower()
+    str1.replace('next', NEXT_QUARTER)
+    str1.replace('eecs', "")
+    return str1
 
 def match_question(chat_text):
     print (chat_text)
     tokenized_chat          =  read_tokenize(chat_text)
-    question_list_parsed    = questionToPattern()
+    # tokenized question list, no punctuation
+    question_list_parsed    =  questionToPattern()
+    curr_question_ind  = 0
 
-    for curr_question_ind, question in enumerate(question_list_parsed):
-        query_args         = []
-        new_arg            = []
+
+    for question in question_list_parsed:
+        query_args, new_arg =[],[]
         question_len       = len(question)
         tokens_len         = len(tokenized_chat)
-        reading_arg        = False
 
         if (fnmatch(chat_text,question_list[curr_question_ind])):
             print ('question matches:', question_list[curr_question_ind])
             qword_ind = 0
-            for word in range (0, tokens_len):
-                if (question[qword_ind] == '*') and (tokenized_chat[word] != question[qword_ind + 1]):
-                    new_arg += tokenized_chat[word]
-                elif (question[qword_ind] == '*') and (tokenized_chat[word] == question[qword_ind + 1]):
-                    new_arg = "".join(new_arg)
-                    new_arg.replace('next', NEXT_QUARTER)
-                    print (type(new_arg))
-                    query_args.append(new_arg)
-
-                    new_arg = []
-                    qword_ind += 2
+            for word_ind in range (0, tokens_len):
+                curr_token  = tokenized_chat[word_ind]
+                curr_qword  = question[qword_ind]
+                # checks if we've reached end of chat_text
+                length_exc  = (qword_ind + 2 >= question_len)
+                if (length_exc):
+                    next_qword = ''
                 else:
+                    next_qword = question[qword_ind+1]
+                print (curr_qword, curr_token, length_exc)
+                if (curr_qword == '*'):
+                    if (length_exc == False and curr_token != next_qword):
+                        new_arg += curr_token
+                    elif (length_exc == False and curr_token == next_qword):
+                        query_args.append(join_lower(new_arg))
+                        new_arg = []
+                        qword_ind += 2
+                    elif (length_exc == True):
+                        new_arg += curr_token
+                        query_args.append(join_lower(new_arg))
+                        
+                else:
+                    print ("not added", question[word_ind])
                     qword_ind += 1
-
-            print ("Query Arg", query_args)
+            print (query_args)
             return (curr_question_ind, query_args)
+        curr_question_ind += 1
     return ("Try another question:\n", question_list)
 
-match_question("Is 348 offered next quarter?")
+match_question("What are the prerequisites for 348?")
